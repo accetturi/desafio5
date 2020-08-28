9ca3cdbf4c773f7aca956b88ee16795b6cbb05ea
Mauricio Accetturi Carvalho <mau.accetturi@gmail.com>
2020-08-28 13:11:13 -0300

    acerto restart

--- restart.sh
+++ restart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 docker stop nestjs
-#docker run -d --rm -p 3000:3000 --name nestjs -v $(pwd)/app:/home/node/app  -v $(pwd)/sqlite.db:/home/node/sqlite.db -e 'CHOKIDAR_USEPOLLING="true"' nestjs
 docker run -d --rm -p 3000:3000 --name nestjs nestjs
 
